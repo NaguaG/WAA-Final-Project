5ddde5a5807a92b6f2e74b1dba5c938b2fa71dc3
Sushil Subedi <sushil173@gmail.com>
2022-09-23 18:34:08 -0500

    adding login and dashboard

--- frontend/zillow/src/components/RoutesWrapper/index.jsx
+++ frontend/zillow/src/components/RoutesWrapper/index.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
+import Login from "../../pages/Auth/Login";
+import Dashboard from "../../pages/Dashboard";
 import HomePage from "../../pages/HomePage";
 import Layout from "../Layout";
 
@@ -9,6 +11,8 @@ const RoutesWrapper = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
