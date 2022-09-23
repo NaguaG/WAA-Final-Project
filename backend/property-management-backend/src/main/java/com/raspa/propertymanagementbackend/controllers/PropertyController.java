2dcd67ea1085ab7a97c6f8cf8fca2f5457ef6756
pradipdhungel <66682622+pradipdhungel@users.noreply.github.com>
2022-09-23 17:59:10 -0500

    Milestone 1

---
+++ backend/property-management-backend/src/main/java/com/raspa/propertymanagementbackend/controllers/PropertyController.java
@@ -0,0 +1,44 @@
+package com.raspa.propertymanagementbackend.controllers;
+
+import com.raspa.propertymanagementbackend.dto.PropertyDTO;
+import com.raspa.propertymanagementbackend.entity.Property;
+import com.raspa.propertymanagementbackend.service.PropertyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/property")
+public class PropertyController {
+
+    private PropertyService propertyService;
+
+    @GetMapping
+    public List<Property> findAll() {
+        return propertyService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Property findById(@PathVariable Long id) {
+        return propertyService.findById(id);
+    }
+
+    @PostMapping
+    public Property save(@RequestBody Property propertyDTO) {
+        return propertyService.save(propertyDTO);
+    }
+
+    @PutMapping("/{id}")
+    public Property update(@PathVariable Long id, @RequestBody Property property) {
+        return propertyService.update(id, property);
+    }
+
+    @DeleteMapping("/{id}")
+    public Property delete(@PathVariable Long id) {
+        return propertyService.delete(id);
+    }
+
+
+}
---
+++ backend/property-management-backend/src/main/java/com/raspa/propertymanagementbackend/entity/Property.java
@@ -0,0 +1,31 @@
+package com.raspa.propertymanagementbackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Boolean isForSell;
+    private Boolean isForRent;
+    private Integer numberOfRooms;
+    private Double price;
+    private String propertyType;
+    private String homeType;
+
+    private String description;
+    @ManyToOne
+    private Location location;
+
+    @OneToMany(mappedBy = "property")
+    private List<Image> images;
+}
