6af5e2699332c5f954ea1199d720ee10a15eca28
asjadanis <asjadanis128@gmail.com>
2022-09-24 15:53:45 -0500

    feat: integrate sign in api

---
+++ frontend/zillow/src/slices/user/userSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { post } from "../../api";
+import jwt_decode from "jwt-decode";
+
+export const loginUser = createAsyncThunk("user/login", async (params) => {
+  try {
+    const response = await post("/api/token", {
+      username: params.email,
+      password: params.password,
+    });
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+});
+
+const initialState = {
+  loading: false,
+  user: {},
+  token: null,
+  error: null,
+  success: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(loginUser.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      const { token } = action.payload;
+      state.loading = false;
+      state.success = true;
+      state.token = token;
+      const userPayload = jwt_decode(token);
+      localStorage.setItem("token", token);
+      state.user = { ...userPayload };
+    });
+    builder.addCase(loginUser.rejected, (state, action) => {
+      state.loading = false;
+      state.success = false;
+      state.error = true;
+    });
+  },
+});
+
+export default userSlice.reducer;
--- frontend/zillow/src/store/index.js
+++ frontend/zillow/src/store/index.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import GlobalSlice from "./slices/sidebar/sidebarSlice";
+import userReducer from "../slices/user/userSlice";
 
 export default configureStore({
   reducer: {
     globalReducer : GlobalSlice.reducer
+  
+    user: userReducer,
   },
 });
