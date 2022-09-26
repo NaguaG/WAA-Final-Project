d72e81bcb2c5f4c2fa68c82c91964be733d62381
asjadanis <asjadanis128@gmail.com>
2022-09-26 13:38:42 -0500

    fix: select handler

--- frontend/zillow/src/pages/HomePage/index.jsx
+++ frontend/zillow/src/pages/HomePage/index.jsx
@@ -11,16 +11,23 @@ import {
 import SearchIcon from "@mui/icons-material/Search";
 import { useState } from "react";
 import ImageCarousal from "../../components/ImageCarousal";
+import PropertyCard from "../../components/PropertyCard";
 
 const HomePage = (props) => {
   const [location, setLocation] = useState("");
   const [propertyType, setPropertyType] = useState("");
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    if (e.target.name === "propertyType") {
+      setPropertyType(e.target.value);
+    } else if (e.target.name === "location") {
+      setLocation(e.target.value);
+    }
+  };
 
   return (
     <Container
-      sx={{ padding: "0px !important", borderRadius: "" }}
+      sx={{ padding: "0px !important", borderRadius: "", marginBottom: "6rem" }}
       component="main"
       maxWidth="xl">
       <ImageCarousal />
@@ -35,6 +42,7 @@ const HomePage = (props) => {
             <FormControl sx={{ m: 1, minWidth: 240 }} size="small">
               <InputLabel id="location-label">Location</InputLabel>
               <Select
+                name="location"
                 labelId="location-label"
                 id="location"
                 value={location}
@@ -50,14 +58,39 @@ const HomePage = (props) => {
             <FormControl sx={{ m: 1, minWidth: 240 }} size="small">
               <InputLabel id="property-label">Property Type</InputLabel>
               <Select
+                name="propertyType"
                 labelId="property-label"
                 id="property"
                 value={propertyType}
                 label="Property Type"
                 onChange={handleChange}>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value="single family home">
+                  Single-Family Home
+                </MenuItem>
+                <MenuItem value="townhome">Townhome</MenuItem>
+                <MenuItem value="bungalow">Bungalow</MenuItem>
+                <MenuItem value="ranch">Ranch</MenuItem>
+                <MenuItem value="condos">Condos</MenuItem>
+                <MenuItem value="victorian">Victorian</MenuItem>
+                <MenuItem value="colonial">Colonial</MenuItem>
+                <MenuItem value="container home">Container Home</MenuItem>
+                <MenuItem value="split level">Split Level</MenuItem>
+                <MenuItem value="houseboat">Houseboat</MenuItem>
+                <MenuItem value="mediterranean">Mediterranean</MenuItem>
+                <MenuItem value="tudor">Tudor</MenuItem>
+                <MenuItem value="craftsman">Craftsman</MenuItem>
+                <MenuItem value="tiny house">Tiny House</MenuItem>
+                <MenuItem value="co op">Co-op</MenuItem>
+                <MenuItem value="cabin">Cabin</MenuItem>
+                <MenuItem value="apartment">Apartment</MenuItem>
+                <MenuItem value="manufactured home">Manufactured Home</MenuItem>
+                <MenuItem value="mobile home">Mobile Home</MenuItem>
+                <MenuItem value="mid century modern style">
+                  Mid-Century Modern Style
+                </MenuItem>
+                <MenuItem value="cape cod">Cape Cod</MenuItem>
+                <MenuItem value="farmhouse">Farmhouse</MenuItem>
+                <MenuItem value="mansion">Mansion</MenuItem>
               </Select>
             </FormControl>
           </Grid>
@@ -68,6 +101,20 @@ const HomePage = (props) => {
               </Button>
             </FormControl>
           </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justifyContent={"center"}
+          sx={{ mt: 3 }}>
+          {[1, 2, 3, 4].map((item) => {
+            return (
+              <Grid item>
+                <PropertyCard />
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </Container>
