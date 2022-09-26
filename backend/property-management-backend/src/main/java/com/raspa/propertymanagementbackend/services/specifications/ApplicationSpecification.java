e79759cd66753578552c576095d6c650bbbad5e4
NaguaG <naranguag@gmail.com>
2022-09-26 13:56:18 -0500

    Application Backend

--- backend/property-management-backend/src/main/java/com/raspa/propertymanagementbackend/services/specifications/ApplicationSpecification.java
+++ backend/property-management-backend/src/main/java/com/raspa/propertymanagementbackend/services/specifications/ApplicationSpecification.java
@@ -30,8 +30,10 @@ public class ApplicationSpecification {
         return new Specification() {
             @Override
             public Predicate toPredicate(Root root, CriteriaQuery query, CriteriaBuilder criteriaBuilder) {
-               Join<Application, Property> applicationPropertyJoin = root.join(Application_.property);
-                return applicationPropertyJoin.get(Property_.location).in(locationId);
+               Join<Application, Location> applicationApplicationJoin = root.join(Application_.property);
+               // TODO Multiple Joins
+//               Join<Application, Location> applicationApplicationJoin = root.join(Application_.property);
+                return applicationApplicationJoin.get(Location_.id).in(locationId);
 
             }
         };
