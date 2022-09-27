40fbaa742442b354e60c22dfcdb370e2d2625420
pradipdhungel <66682622+pradipdhungel@users.noreply.github.com>
2022-09-26 19:57:33 -0500

    Todo for property filter in index.jsx

--- frontend/zillow/src/store/index.js
+++ frontend/zillow/src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import GlobalSlice from "./slices/sidebar/sidebarSlice";
 import userReducer from "./slices/user/userSlice";
+import propertyReducer from "./slices/property/propertySlice";
 import usersReducer from "./slices/users/usersSlice";
 
 export default configureStore({
   reducer: {
     globalReducer: GlobalSlice.reducer,
     user: userReducer,
+    properties:propertyReducer
     users: usersReducer
   },
 });
