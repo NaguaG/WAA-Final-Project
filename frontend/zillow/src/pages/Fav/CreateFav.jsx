04374984e34a5910242139737906f56397f9cdf5
Sushil Subedi <sushil173@gmail.com>
2022-10-01 01:00:38 -0500

    favList CRUD done

--- frontend/zillow/src/pages/Fav/CreateFav.jsx
+++ frontend/zillow/src/pages/Fav/CreateFav.jsx
@@ -1,22 +1,52 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material'
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { createFavList, editFavList, fetchFavList } from '../../store/slices/fav/favSlice';
+
 
 export default function CreateFav(props) {
+  const dispatch = useDispatch();
   const favListData = useSelector((state) => state.favLists );
+  const [name, setName] = useState('')
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    if(!favListData.isFirst)
+      setName(props.item.name)
+  },[])
   
+
   const handleClose = () =>{
     props.setOpen(!props.open); 
   }
   
+  const onNameChange = (event) =>{
+    setError( event.target.value ? false : true);
+    setName(event.target.value)
+    // props.setOpen(!props.open); 
+  }
+  
   const handleConfirmSubmit = (item) =>{
+    if(name == null || name == ''){
+      setError(true);
+      return;
+    }
+    let data = new FormData();
+    data.name = name;
     if(favListData.isFirst){
-      console.log('====================================');
-      console.log('During Adding');
-      console.log('====================================');
+      dispatch(createFavList(data)).then(res => {
+        dispatch(fetchFavList());
+        props.setOpen(!props.open);
+      });
     } else {
       // edit
-      props.onCreateConfirmClicked(item)
+      data.id = item.id
+      console.log('data', data);
+      dispatch(editFavList(data)).then(res => {
+        dispatch(fetchFavList());
+        props.setOpen(!props.open);
+      });
+      // props.onCreateConfirmClicked(data)
     }
   }
 
@@ -36,7 +66,12 @@ export default function CreateFav(props) {
             type="text"
             fullWidth
             variant="standard"
-            value={favListData.isFirst ? '' : props.item.name}
+            value={name}
+            onChange={onNameChange}
+            color={ error ?  "warning" : ""}
+            focused={ error }
+            error={ error }
+            helperText={error ? "Required." : ''}
           />
         </DialogContent>
         <DialogActions>
--- frontend/zillow/src/store/slices/fav/favSlice.js
+++ frontend/zillow/src/store/slices/fav/favSlice.js
@@ -1,9 +1,30 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { get } from "../../../api";
+import { deleteRequest, get, post, put } from "../../../api";
 
 export const fetchFavList = createAsyncThunk("fetch/fav", 
   async(params) => {
     const res = await get('/api/fav-lists');
+    return res.data;
+  }
+)
+
+export const createFavList = createAsyncThunk("create/fav", 
+  async(params) => {
+    const res = await post('/api/fav-lists', params);
+    return res.data;
+  }
+)
+
+export const editFavList = createAsyncThunk("edit/fav", 
+  async(params) => {
+    const res = await put('/api/fav-lists/'+params.id, params);
+    return res.data;
+  }
+)
+
+export const deleteFavList = createAsyncThunk("delete/fav", 
+  async(params) => {
+    const res = await deleteRequest('/api/fav-lists/'+params);
     return res.data;
   }
 )
@@ -31,6 +52,27 @@ const favSlice = createSlice({
       state.success = true;
     });
     builder.addCase(fetchFavList.pending, (state, action) => {
+      state.loading = true;
+    })
+    builder.addCase(createFavList.fulfilled, (state, action) => {
+      state.favList.push(action.payload);
+      state.success = true;
+    });
+    builder.addCase(createFavList.pending, (state, action) => {
+      state.loading = true;
+    })
+    builder.addCase(editFavList.fulfilled, (state, action) => {
+      // state.favList = action.payload;
+      state.success = true;
+    });
+    builder.addCase(editFavList.pending, (state, action) => {
+      state.loading = true;
+    })
+    builder.addCase(deleteFavList.fulfilled, (state, action) => {
+      // state.favList = action.payload;
+      state.success = true;
+    });
+    builder.addCase(deleteFavList.pending, (state, action) => {
       state.loading = true;
     })
   }
