d6857500535939c654db665da26e9444d8fee0e7
asjadanis <asjadanis128@gmail.com>
2022-10-01 18:24:33 -0500

    fix: typos

--- frontend/zillow/src/store/slices/property/propertySlice.js
+++ frontend/zillow/src/store/slices/property/propertySlice.js
@@ -1,57 +1,56 @@
 import { Satellite } from "@mui/icons-material";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchProperties = createAsyncThunk("properties/getAll", async (params) => {
+export const fetchProperties = createAsyncThunk(
+  "properties/getAll",
+  async (params) => {
     try {
       const response = await get("/api/properties");
       return response.data;
     } catch (err) {
-        throw err;
+      throw err;
     }
-  });
-const initialState={
-    properties: [],
-    loading:false,
-    error:null,
-    success:null,
-}
+  }
+);
+const initialState = {
+  properties: [],
+  loading: false,
+  error: null,
+  success: null,
+};
 
 //==================
-const param={
-    name:'property',
-    initialState,
-    reducers:{
-        loadProperty:(state)=>{
-        }
-    },
+const param = {
+  name: "property",
+  initialState,
+  reducers: {
+    loadProperty: (state) => {},
+  },
 
-    extraReducers:(builder)=>{
-      builder.addCase(fetchProperties.pending, (state)=>
-      {
-        state.loading=true;
-        state.success=false;
-        state.error=false;
-        state.properties=[];
-      });
+  extraReducers: (builder) => {
+    builder.addCase(fetchProperties.pending, (state) => {
+      state.loading = true;
+      state.success = false;
+      state.error = false;
+      state.properties = [];
+    });
     //========================================================
 
-    builder.addCase(fetchProperties.fulfilled,(state,action)=>{
-      state.loading=false;
-      state.success=true;
-      state.error=false;
-      state.properties=action.payload;
+    builder.addCase(fetchProperties.fulfilled, (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.error = false;
+      state.properties = action.payload;
     });
 
-    builder.addCase(fetchProperties.rejected, (state)=>{
-      state.properties=[];
-      state.loading=false;
-      state.success=false;
-      state.error=true;
-    })
-
-}}
-const propertyDashboardSlice=createSlice(param);
+    builder.addCase(fetchProperties.rejected, (state) => {
+      state.properties = [];
+      state.loading = false;
+      state.success = false;
+      state.error = true;
+    });
+  },
+};
+const propertyDashboardSlice = createSlice(param);
 
 export default propertyDashboardSlice.reducer;
-
-
