731753c5b97c066295842d91260b1e9be60634bb
pradipdhungel <66682622+pradipdhungel@users.noreply.github.com>
2022-10-01 19:01:09 -0500

    Todo for property filter in index.jsx

--- frontend/zillow/src/store/slices/property/propertySlice.js
+++ frontend/zillow/src/store/slices/property/propertySlice.js
@@ -1,10 +1,25 @@
 import { Satellite } from "@mui/icons-material";
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { get } from "../../../api";
 
 export const fetchProperties = createAsyncThunk("properties/getAll", async (params) => {
     try {
-      const response = await get("/api/properties");
-      return response.data;
+        let url = "/api/properties";
+        let query = [];
+        if(params && params.valueHomeType) query.push(`homeType=${params.valueHomeType}`); //todos
+        if(params && params.valueLocation) query.push(`location=${params.valueLocation}`);
+        if(params && params.valuePrice) query.push(`price=${params.valuePrice}`);
+        if(params && params.valueNumberOfRooms) query.push(`numberOfRooms=${params.valueNumberOfRooms}`);
+        if(params && params.valuePropertyType) query.push(`propertyType=${params.valuePropertyType}`);
+
+        for(let i = 0; i < query.length; i++){
+            if(i === 0) url += "?" + query[i];
+            else url += "&" + query[i];
+        }
+
+        const response= await get(url);
+        console.log("PRADIP", response);
+        return response.data.content;
     } catch (err) {
         throw err;
     }
